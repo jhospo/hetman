aeafc9914cd4367ee7da6a75eeb5a5dae8fdd3c9
jhospo <janhospodarysko11@gmail.com>
2025-03-14 22:12:05 +0100

    .

--- main.cpp
+++ main.cpp
@@ -6,32 +6,54 @@
 using namespace std;
 const int n = 8;
 
-bool possible(int row, int col, const vector<pair<int,int>>& Queen) {
+bool possible(int row, int col, const vector<pair<int, int>> &Queen) {
     for (int i = 0; i < Queen.size(); i++)
         if (Queen[i].first == row || Queen[i].second == col || abs(Queen[i].first - row) == abs(Queen[i].second - col))
             return false;
     return true;
 }
 
+bool solve(int col, vector<pair<int, int>> &Queen) {
+    if (col == n)
+        return true;
+    if (col == 0) {
+        int RRow = rand() % n;
+        Queen.push_back({RRow, col});
+        cout << "start: " << RRow << " " << col << endl;
+        if (solve(col + 1, Queen))
+            return true;
+        cout << "back col: " << col << endl;
+        Queen.pop_back();
+        return false;
+
+    }
+
+    for (int row = 0; row < n; row++) {
+        cout << "try: " << row << " " << col << endl;
+        if (possible(row, col, Queen)) {
+            Queen.push_back({row, col});
+            cout << "placed: " << row << " " << col << endl;
+            if (solve(col + 1, Queen))
+                return true;
+            Queen.pop_back();
+        }
+    }
+    cout << "back to: " << col - 1 << endl;
+    return false;
+
+}
+
 int main() {
-    vector<pair<int,int>> Queen;
-    Queen.push_back({0, 0});
+    srand(time(NULL));
+    vector<pair<int, int>> Queen;
+    if (solve(0, Queen)) {
+        cout << "SUKCES";
+        for (int i = 0; i < Queen.size(); i++) {
+            cout << "(" << Queen[i].first << ", " << Queen[i].second << ") ";
+        }
+        cout << endl;
 
-    if (possible(1, 2, Queen)) {
-        cout << "Moozna" << endl;
-        Queen.push_back({1, 2});
-    } else {
-        cout << "Nie" << endl;
-    }
 
-    if (possible(1, 1, Queen)) {
-        cout << "tak" << endl;
-    } else {
-        cout << "Nie" << endl;
     }
-    for (int i = 0; i < Queen.size(); i++) {
-        cout << Queen[i].first << Queen[i].second << endl;
-    }
-
     return 0;
 }
